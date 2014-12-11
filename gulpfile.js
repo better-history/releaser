adb5fad219d7241a5a8876425ad16d49fa3a955e
Roy Kolak <roy.kolak@gmail.com>
2014-12-11 00:13:06 -0600

    update gulp and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,27 +8,55 @@ var minifyHtml = require('gulp-minify-html');
 var path = require("path");
 var es = require("event-stream");
 
-gulp.task('default', function() {});
+gulp.task('default', ['copy', 'templatify', 'usemin', 'sloppyCopy']);
 
-gulp.task('usemin', function () {
-  return gulp.src('better-history/index.html')
-    .pipe(usemin({
-      css: [minifyCss(), 'concat'],
-      js: [uglify()]
-    }))
-    .pipe(gulp.dest('build/'));
+gulp.task('copy', function(){
+  // We must ditch the git repo due to issues when usemin runs. It was not picking up a updated file in the git submodule for some reason.
+  gulp.src(['./better-history/**/*'])
+  .pipe(gulp.dest('temp'));
+
+  gulp.src([
+    './temp/_locales/**/*',
+    './temp/manifest.json',
+    './temp/images/**/*'
+  ], {base: './temp'})
+  .pipe(gulp.dest('build'));
+
+  gulp.src(['./scripts/config.js'])
+  .pipe(gulp.dest('temp/scripts'));
 });
 
-gulp.task('templatify', function(){
-  gulp.src('better-history/templates/*.html')
+gulp.task('templatify', function(cb){
+  gulp.src('temp/templates/*.html')
     .pipe(minifyHtml())
     .pipe((function() {
       return es.map(function(file, callback) {
         var name = path.basename(file.path);
-        file.contents = new Buffer("BH.Templates['" + name + "'] = \"" + String(file.contents) + '"');
+        file.contents = new Buffer("BH.Templates['" + name + "'] = '" + String(file.contents) + "';");
         callback(null, file);
       });
     })())
-    .pipe(concat('template.js'))
-    .pipe(gulp.dest('build/scripts'));
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('temp/scripts/views/'));
+    cb();
 });
+
+gulp.task('usemin', ['templatify'], function (cb) {
+  gulp.src('temp/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat'],
+     js: []
+    }))
+    .pipe(gulp.dest('build/'));
+    cb();
+});
+
+// This is the solution to making sure background scripts are present. Sloppy copy all scripts into the release. Sucks...
+gulp.task('sloppyCopy', ['usemin'], function(){
+  gulp.src(['./temp/scripts/**/*'], {base: './temp'})
+  .pipe(gulp.dest('build'));
+
+  gulp.src(['./temp/bower_components/**/*'], {base: './temp'})
+  .pipe(gulp.dest('build'));
+});
+
